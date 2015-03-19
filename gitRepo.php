bb63b1cb8c55a95b113e9c409f26b7037f718c60
root <root@ip-172-31-19-28.us-west-2.compute.internal>
2015-03-19 02:10:01 -0400

    added signup/login page (users can only use the web app  if logged in)

--- gitRepo.php
+++ gitRepo.php
@@ -1,21 +1,53 @@
+<?php
+require('include/class.database.inc.php');
+require('include/class.session.inc.php');
+require('include/class.user.inc.php');
+
+session_start();
+$newsess = new Session;
+$derp = $newsess->_read(session_id());
+// echo "<pre>";
+// print_r($derp);
+// echo "</pre>";
+session_decode($derp['data']);
+// print_r($_SESSION);
+
+if(!isset($_SESSION['userid'])){ //if login in session is not set
+    header("Location: http://54.148.32.230");
+}
+
+$newUser = new User;
+$userInfo=$newUser->getUserData($_SESSION['userid']);
+
+// echo json_encode($userInfo);
+$username=$userInfo['username'];
+$usercolor=$userInfo['usercolor'];
+?>
 <html>
 <head>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 <script src="js/mustache.min.js"></script>
-<script src="js/index.js"></script>
+<script src="js/gitRepo.js"></script>
 <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="css/animate.css">
-<link rel="stylesheet" href="css/index.css">
+<link rel="stylesheet" href="css/gitRepo.css">
 </head>
 <body>
 <div class='bodyContainer'>
 	<div class="countBlock hide">
 		<div class="totalCount"></div>
 	</div>
+	<div class="logoutBlock">
+		<div class="logoutTxt">
+			<div id="logoutUser">
+				logout
+			</div>
+		</div>
+	</div>
 	<div class='form'>
 		<div class="formContent">
 			<div class="topForm">
-				<input type='text' name='query' autofocus="autofocus" id='searchQ' class="main" placeholder="SEACH GITHUB REPOSITORIES" autocomplete="off" size="35" style="display:inline-block">
+				<input type='text' name='query' autofocus="autofocus" id='searchQ' class="queryPlaceholder <?php echo $usercolor?>" placeholder="SEACH GITHUB REPOSITORIES" autocomplete="off" size="35" style="display:inline-block">
 				<div class="icons">
 					<div class="pointer advance"><i class="fa fa-angle-down"></i></div><div class="pointer"><i class="fa fa-arrow-right" id="retrieve"></i></div>
 				</div>
@@ -53,9 +85,17 @@
 		</div>
 	</div>
 	<div class="queryResults">
-		<div class="resultContainer">
+		<div class="resultContainer <?php echo $usercolor?>">
 		</div>
 	</div>
 </div>
+<script type="text/javascript">
+	var usercolor=<?php echo json_encode($usercolor);?>;
+	$(".icons i").mouseenter(function(){
+		$(this).addClass(usercolor);
+	}).mouseleave(function() {
+		$(this).removeClass(usercolor);
+	});
+</script>
 </body>
 </html>
--- js/index.js
+++ js/index.js
@@ -1,168 +1,75 @@
 $(document).ready(function(){
-	var urlRequest='';
-	var q;
-	var In;
-	var language;
-	var stars;
-	var queryResult;
-	var pageNumber=1;
-	var ready=true;
-	var totalCount;
-	var pageLimit;
-	// SUBMIT INITIAL QUERY
-	$('#retrieve').on("click", function(){
-		$('.resultContainer').empty();
-		q=$('#searchQ').val();
-		In=$('#IN').val();
-		language=$('#language').val();
-		stars=$('#numberOfStars').val().trim();
-		$('.form').addClass('slideFormUp');
-		if(q===''){return 0}
+	$('#submitForm').on("click", function(){
+		if($('#username').val()==='' ||
+			$('#password').val()===''){
+			return false;
+		}
+		var username=$('#username').val();
+		var password=$('#password').val();
+		var color=$('#colorValue').val();
+		var whichone;
+		if($('#signUp').hasClass('active')){
+			whichone="signup";
+		}
 		else{
-			pageNumber=1;
-			ready=true;
-			totalCount=0;
-			pageLimit=0;
-			urlRequest="https://api.github.com/search/repositories?&q=";
-			urlRequest=urlRequest+q;
-			if(In!=='')urlRequest=urlRequest+'+in:'+In;
-			if(language!=='') urlRequest=urlRequest+'+language:'+language;
-			if(stars!=='') urlRequest=urlRequest+'+stars:'+stars;
-			urlRequest=urlRequest+'&per_page=20';
-			urlRequest=urlRequest+'&page='+pageNumber;
-			pageNumber++;
-			$.ajax({
-				url: urlRequest,
-				dataType: 'json',
-				success: function(data)
-				{
-					console.log(data);
-					console.log(data.total_count);
-					totalCount=data.total_count;
-					pageLimit=Math.ceil(totalCount/20);
-					// console.log(pageLimit);
-					$.each(data.items, function(){
-						queryResult=data.items;
-					});
-					// console.log(queryResult);
-					$('.totalCount').text("Found "+commaSeparateNumber(totalCount)+" repositories");
-					$('.countBlock').show();
-					resetInputs();
-					if(totalCount===0){
-						$('.resultContainer').append('NO RESULTS FOUND');
-						return 0;
+			whichone="signin";
+		}
+		$.ajax({
+			url: '../ajax/userFunctions.php',
+			dataType: 'json',
+			type: 'post',
+			data: {
+				username: username,
+				password: password,
+				color: color,
+				type: whichone
+			},
+			success: function(data)
+			{
+				if(data.status===0){
+					if(whichone==="signin"){
+						console.log('Failed login: '+data.personalMsg);
 					}
-					loadResult(queryResult);
-				},
-				error: function(request, status, error){
-					console.log('ERROR: '+ request +' failed. status: '+status+'. '+ error);
+					else{
+						console.log('Failed Signup: '+data.personalMsg);
+						$('.usernameError').removeClass('hide');
+						$('.passwordError').addClass('hide');
+						$('.usernameErrorTxt').text(data.personalMsg);
+					}
 				}
-			});
-		}
-	});
-
-	//INITIAL LOAD
-	function loadResult(dataItems){
-		$.get('htmlTemplates/resultBlock.mst', function(template){
-			var rendered=Mustache.render(
-				template,
-				{
-					result: dataItems
+				else{
+					console.log(data.personalMsg);
+					if(data.personalMsg==="User Does Not Exist"){
+						$('.usernameError').removeClass('hide');
+						$('.passwordError').addClass('hide');
+						$('.usernameErrorTxt').text(data.personalMsg);
+					}else if(data.personalMsg==="Wrong Password"){
+						$('.passwordError').removeClass('hide');
+						$('.usernameError').addClass('hide');
+						$('.passwordErrorTxt').text(data.personalMsg);
+					}else if(data.personalMsg==="Welcome Back"){
+						alert('going in');
+						window.location.replace("http://54.148.32.230/gitRepo.php");
+					}
 				}
-			);
-			$('.resultContainer').append(rendered);
+			},
+			error: function(request, status, error){
+				console.log('ERROR: '+ request +' failed. status: '+status+'. '+ error);
+			}
 		});
-	}
-
-
-	function moreResults(){
-		if(urlRequest==='') return 0;
-		urlRequest=urlRequest.replace(/&page=.*/,'&page='+pageNumber);
-		if(ready===true && pageNumber<=pageLimit){
-			ready=false;
-			$.ajax({
-				url: urlRequest,
-				dataType: 'json',
-				success: function(data)
-				{
-					console.log(data);
-					$.each(data.items, function(){
-						queryResult=data.items;
-					});
-					// console.log(queryResult);
-					loadResult(queryResult);
-					pageNumber++;
-					ready=true;
-				},
-				error: function(request, status, error){
-					console.log('ERROR: '+ request +' failed. status: '+status+'. '+ error);
-				}
-			});
-		}
-	}
-
-	function loadMore(){
-		// debugging
-		// console.log(urlRequest);
-		// console.log(ready);
-		// console.log(pageNumber);
-		moreResults();
-		$(window).bind('scroll', bindScroll);
-	}
-
-	function bindScroll(){
-		if($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
-			$(window).unbind('scroll');
-			loadMore();
-		}
-	}
-
-	//PAGINATION
-	$(window).scroll(bindScroll());
-	
-	function resetInputs(){
-		$('#IN').val('');
-		$('#language').val('');
-		$('#numberOfStars').val('')
-	}
-	function resetVariables(){
-		urlRequest='';
-		q='';
-		In='';
-		language='';
-		stars='';
-		queryResult='';
-		pageNumber=1;
-		ready=true;
-		totalCount=0;
-		pageLimit=0;
-	}
-	function commaSeparateNumber(val){
-		while (/(\d+)(\d{3})/.test(val.toString())){
-			val = val.toString().replace(/(\d+)(\d{3})/, '$1'+','+'$2');
-		}
-		return val;
-	}
-
-	// EVENT HANDLING
-	$('.advance').on("click", "i", function(){
-		if($('.advanceSearch').hasClass('hide')){
-			$('.advanceSearch').removeClass('fadeOut');
-			$('.advanceSearch').removeClass('hide');
-			$('.advanceSearch').addClass('fadeIn');
-			$('.queryResults').addClass('moveDown');
-		}
-		else{
-			$('.advanceSearch').removeClass('fadeIn');
-			$('.advanceSearch').addClass('fadeOut');
-			setTimeout(function(){ $('.advanceSearch').addClass('hide'); $('.queryResults').removeClass('moveDown');}, 500);
-		}
 	});
-	$('#searchQ').keypress(function (e) {
-		var key = e.which;
-		if(key == 13){
-			$('#retrieve').trigger('click');
-			return false;  
-		}
+	$('#signUp').on("click", function(){
+		$('#signIn').removeClass('active');
+		$('#signUp').addClass('active');
+		$('#userForm').removeClass('hide');
+		$('#pickColor').removeClass('hide');
+		$('.usernameError').addClass('hide');
+		$('.passwordError').addClass('hide');
+	});
+	$('#signIn').on("click", function(){
+		$('#signUp').removeClass('active');
+		$('#signIn').addClass('active');
+		$('#pickColor').addClass('hide');
+		$('#userForm').removeClass('hide');
 	});
 });
